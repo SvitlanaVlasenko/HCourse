be2912c8b0f76aae5aab290a552695c01ac9db78
svlasenko <svetlana.vlasenko@microfocus.com>
2021-10-22 22:26:05 +0300

    Homework3

--- src/com/company/homework3/Person.java
+++ src/com/company/homework3/Person.java
@@ -4,11 +4,12 @@ package com.company.homework3;
 
 import java.util.Arrays;
 
-public class Person{
+public class Person {
     public static void main(String[] args) {
 /**2) Задан массив случаных чисел типа int в котором есть положительные и отрицательные числа.
 
  Написать метод который вернет массив квадратов этих чисел, отсортированных по возрастанию.*/
+        System.out.println(getPersons());
 
         int arr[] = new int[10];
         for (int i = 0; i < arr.length; i++) {
@@ -18,26 +19,61 @@ public class Person{
         System.out.print("\nSorted numbers: \n");
         Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "  ");
+            System.out.print(Math.pow(arr[i], 2) + "  ");
         }
 
     }
-    /**1) Создать класс Person у котороего есть 3 поля - фамилия, имя, отчество.
-     Создать 2 конструктора - одни на все поля,
-     во второй задается строка типа "Иванов Иван Иванович",
-     он ее должен распарсить и задать значения полям.*/
-    String firstName, secondName, lastName;
 
-    Person(String firstName, String secondName, String lastName) {
+
+    /**
+     * 1) Создать класс Person у котороего есть 3 поля - фамилия, имя, отчество.
+     * Создать 2 конструктора - одни на все поля,
+     * во второй задается строка типа "Иванов Иван Иванович",
+     * он ее должен распарсить и задать значения полям.
+     */
+    protected String firstName;
+    protected String secondName;
+    protected String lastName;
+
+    public Person(String firstName, String secondName, String lastName) {
         this.firstName = firstName;
         this.secondName = secondName;
         this.lastName = lastName;
     }
-    public Person getPersons(){
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public static Person getPersons() {
         Person persons = new Person("Ivanov", "Ivan", "Ivanovich");
+        persons.setFirstName("Ivanov");
+        persons.setSecondName("Ivan");
+        persons.setLastName("Ivanovich");
         return persons;
     }
 }
+
 
 
 
