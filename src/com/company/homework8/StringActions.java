839b0632369d5c47c5379b2456447df0357e3117
svlasenko <svetlana.vlasenko@microfocus.com>
2021-10-24 11:59:41 +0300

    Homework8

---
+++ src/com/company/homework8/StringActions.java
@@ -0,0 +1,48 @@
+package com.company.homework8;
+
+public class StringActions {
+
+    public static void main(String[] args) {
+
+        System.out.println(stringReverse());
+        System.out.println(findSymbolOccurance());
+        System.out.println(isPalindrome());
+    }
+
+    public static int findSymbolOccurance() {
+        String str = "abs";
+        int occurrences = 0;
+        for (char chr : str.toCharArray()) {
+             if (chr == '\u0430') {
+                occurrences++;
+            }
+        }
+        return occurrences;
+    }
+
+    //public static findWordPosition() {
+    //    String source = "Apollo";
+    //     String target = "pollo";
+    //}
+
+    public static String stringReverse() {
+        String strn = "Hello";
+        //return new StringBuilder(strn).reverse().toString();
+        //reverse or we can use charAt
+        String result = "";
+        for (int i = 0; i < strn.length(); i++) {
+            result = strn.charAt(i) + result;
+        }
+        return result;
+    }
+
+    public static boolean isPalindrome() {
+        String pol = "ERRE";
+        for (int i = 0, j = pol.length() - 1; i < j; i++, j--) {
+            if (pol.charAt(i) != pol.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
