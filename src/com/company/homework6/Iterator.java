7637b4d0db2708c7bb2e7ad5445df2c90dcce2c1
svlasenko <svetlana.vlasenko@microfocus.com>
2021-10-24 17:39:09 +0300

    Homework6AddIntegrator

---
+++ src/com/company/homework6/Iterator.java
@@ -0,0 +1,21 @@
+package com.company.homework6;
+
+
+public class Iterator {
+    private int iterator;
+    private int[][] arr;
+
+    public Iterator(int[][] arr) {
+        // TODO
+        this.arr = arr;
+        this.iterator = 0;
+    }
+
+    public Object next() {
+        return arr[iterator++];
+    }
+
+    protected boolean hasNext(Iterator iterator) {
+        return (this.iterator < arr.length);
+    }
+}
--- src/com/company/homework6/Main.java
+++ src/com/company/homework6/Main.java
@@ -3,6 +3,17 @@ package com.company.homework6;
 public class Main {
     public static void main(String[] args) {
         System.out.println(GetFigure());
+
+
+        int[][] arr = {
+                {1, 3, 5},
+                {1, 3, 7},
+                {2, 2, 3, 6}
+        };
+        Iterator iterator = new Iterator(arr);
+        while (iterator.hasNext(iterator)) {
+            System.out.print("Integrator:"+ iterator.next().toString() + " ");
+        } // 1, 1, 2, 2, 3, 3, 3, 5, 6, 7
     }
 
     public static double GetFigure() {
@@ -23,5 +34,7 @@ public class Main {
                 new Circle(), new Square(), new Triangle()
         };
     }
+
+
 }
 
