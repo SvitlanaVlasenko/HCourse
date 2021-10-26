352f493b2b1c7619b1ea07f958dc998a9c943a01
svlasenko <svetlana.vlasenko@microfocus.com>
2021-10-26 20:01:51 +0300

    Homewor3rework

--- src/com/company/homework3/Person.java
+++ src/com/company/homework3/Person.java
@@ -25,26 +25,32 @@ public class Person {
     }
 
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getSecondName() {
+
         return secondName;
     }
 
     public void setSecondName(String secondName) {
+
         this.secondName = secondName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
