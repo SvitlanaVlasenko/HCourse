4f91bc407ad73de19710882e928f4f0689261a28
svlasenko <svetlana.vlasenko@microfocus.com>
2022-01-12 17:06:29 +0200

    HW21

---
+++ src/com/company/homework21/ProjectTestRunner.java
@@ -0,0 +1,9 @@
+package com.company.homework21;
+
+public class ProjectTestRunner {
+    public static void main(String[] args){
+        //new TestRunner().run("homework21.SimpleMathLibraryTest");
+       // new TestRunner().run(SimpleMathLibrary.class);
+       // new TestRunner().run("com.company.homework21");
+    }
+}
---
+++ src/com/company/homework21/TestResultParser.java
@@ -0,0 +1,4 @@
+package com.company.homework21;
+
+public class TestResultParser {
+}
